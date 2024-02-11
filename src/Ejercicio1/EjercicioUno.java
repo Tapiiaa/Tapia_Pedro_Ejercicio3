6d31b2951b37c5b1a2c7a3968f347deb3b682291
Tapiiaa <ptapilob@myuax.com>
2024-02-12 00:18:13 +0100

    Ejercicio 1 terminado

--- src/Ejercicio1/EjercicioUno.java
+++ src/Ejercicio1/EjercicioUno.java
@@ -6,13 +6,27 @@ public class EjercicioUno {
         double sentimiento = 0;
         double umbralPositivo = 0.5;
         double umbralNegativo = -0.5;
-        if(sentimiento > umbralPositivo) {
+        if (sentimiento > umbralPositivo) {
             return "Positivo";
-        } else if(sentimiento < umbralNegativo) {
+        } else if (sentimiento < umbralNegativo) {
             return "Negativo";
         } else {
             return "Neutral";
         }
     }
 
+    public static void main(String[] args) {
+        Publica[] Texto = new Publica[3];
+        new Publica("Felicidad");
+        new Publica("Tristeza");
+        new Publica("Indiferencia");
+        for (Publica texto : Texto) {
+
+            String sentimiento = analizarSentimiento(texto.getTexto());
+            System.out.println("Texto: " + texto.getTexto() + " Sentimiento: " + sentimiento);
+        }
+    }
 }
+
+
+
---
+++ src/Ejercicio1/Publica.java
@@ -0,0 +1,12 @@
+package Ejercicio1;
+
+public class Publica {
+    private String texto;
+
+    public Publica(String texto) {
+        this.texto = texto;
+    }
+    public String getTexto() {
+        return texto;
+    }
+}

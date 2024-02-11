d56b5bf2a406bb5165316cafaa31c20ec841c3b9
Tapiiaa <ptapilob@myuax.com>
2024-02-11 23:31:44 +0100

    Creacion "Texto" y actualizacion cambios

--- src/Ejercicio1/EjercicioUno.java
+++ src/Ejercicio1/EjercicioUno.java
@@ -1,4 +1,18 @@
 package Ejercicio1;
+import java.util.Scanner;
 
 public class EjercicioUno {
+    public static String analizarSentimiento(String texto) {
+        double sentimiento = 0;
+        double umbralPositivo = 0.5;
+        double umbralNegativo = -0.5;
+        if(sentimiento > umbralPositivo) {
+            return "Positivo";
+        } else if(sentimiento < umbralNegativo) {
+            return "Negativo";
+        } else {
+            return "Neutral";
+        }
+    }
+
 }

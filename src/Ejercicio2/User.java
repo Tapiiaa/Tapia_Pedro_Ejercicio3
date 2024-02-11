a2ada3d32cef7fa10c0b30f07e607ea112cfed00
Tapiiaa <ptapilob@myuax.com>
2024-02-12 00:23:16 +0100

    Ejercicio 2, creacion de USER

---
+++ src/Ejercicio2/User.java
@@ -0,0 +1,14 @@
+package Ejercicio2;
+
+public class User {
+    private String nombre;
+    private String numID;
+
+    public User(){
+        this.nombre=nombre;
+        this.numID=numID;
+    }
+    public String getNumID(){
+        return this.numID;
+    }
+}

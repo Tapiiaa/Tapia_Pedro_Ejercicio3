21abcd3d12f84a597b2772c61293f44c358ba623
Tapiiaa <ptapilob@myuax.com>
2024-02-12 08:36:01 +0100

    Creacion Rendimiento

---
+++ src/Ejercicio5/Rendimiento.java
@@ -0,0 +1,10 @@
+package Ejercicio5;
+
+public class Rendimiento {
+    int CPU;
+    int memoria;
+    public Rendimiento(int CPU, int memoria) {
+        this.CPU = CPU;
+        this.memoria = memoria;
+    }
+}
